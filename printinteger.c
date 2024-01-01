5d20a4a06a93e26552d28eab45bc91de8fa0c7c3
palparul <pbichhele@gmail.com>
2024-01-01 19:20:30 +0530

    code to print integer

---
+++ printinteger.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    
+    printf("Enter the value :\n");
+    
+    scanf("%d",&x);
+    
+    printf("print the given integer value :\n %d",x);
+
+    return 0;
+}
